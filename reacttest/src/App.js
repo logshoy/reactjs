5f722d74b585ec892889037d4f6e4f4de10f874f
logshoy <logshoy@gmail.com>
2020-05-25 15:13:50 +0500

    lesson 20

--- reacttest/src/App.js
+++ reacttest/src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car/Car'
 
-function App() {
+class App extends React.Component {
+  state = {
+    cars:[
+      {name:'Ford', year: 2018},
+      {name:'Audi', year: 2016},
+      {name:'Mazda1', year: 2010}
+    ],
+    pageTitle: "React Components",
+    showCars: false
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({
+      cars
+    })
+  }
+
+  toogleCarsHandler = () => {
+
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  deleteHandler(index) {
+    let cars = this.state.cars.concat()
+
+    cars.splice(index, 1)
+
+    this.setState({cars})
+  }
+
+  render () {
+  const divStyle = {
+    textAlign : "center"
+  }
+
+  let cars = null
+
+  if (this.state.showCars) {
+    cars = this.state.cars.map((car,index)=> {
+      return (
+        <Car
+        key={index}
+          name={car.name}
+          year={car.year}
+          onDelete={this.deleteHandler.bind(this, index)}
+          onChangeName={event => this.onChangeName(event.target.value, index)}
+        />
+      )
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <h1> {this.state.pageTitle} </h1>
+      <button 
+      onClick={this.toogleCarsHandler}
+      >Toogle Cars</button>
+
+    { cars }
+
+    
     </div>
   );
+  }
 }
 
 export default App;
